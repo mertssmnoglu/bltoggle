ea4604492a0274477b76b7b6ce55a192894cc0c2
Mert Şişmanoğlu <mert190737fb@gmail.com>
2021-07-19 15:22:06 +0300

    Add bluetooth status messages.

--- bltoggle.sh
+++ bltoggle.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
-
+turnedOnText='\033[1mBluetooth\033[0;34m \033[1mturned on\033[0m'
+turnedOffText='\033[1mBluetooth\033[0;34m \033[1mturned off\033[0m'
 function toggleBluetooth {
     if rfkill list bluetooth|grep -q 'yes$';then
         rfkill unblock bluetooth;
+        echo -e $turnedOnText
     else
         rfkill block bluetooth;
+        echo -e $turnedOffText
     fi
 }
 toggleBluetooth
